6b1afe72c1fd7793c8859064062937eccd3733cb
QAVshivtsev <159697104+QAVshivtsev@users.noreply.github.com>
2024-02-23 03:49:57 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 13_676;
+        int bonusMinimum = 20;
+        int charge = cost / bonusMinimum;
+        System.out.println(charge);
+    }
+}
